1a39f389c5b331a35d81fa03f5441a5c4e345784
mikhail chigurov <vip.moneta95@mail.ru>
2019-09-09 19:17:40 +0700

    DropGame.

---
+++ core/src/info/fandroid/drop/Drop.java
@@ -0,0 +1,35 @@
+package info.fandroid.drop;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Admin on 18.04.2017.
+ */
+
+public class Drop extends Game {
+
+    SpriteBatch batch;
+    BitmapFont font;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch ();
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        batch.dispose();
+        font.dispose();
+    }
+}
